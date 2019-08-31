224834b19872fe7bd7bb911de91a62ab3b47e6cc
nayanmehta03 <53710117+nayanmehta03@users.noreply.github.com>
2019-08-31 10:58:35 +0530

    Update Stark.java

--- Stark.java
+++ Stark.java
@@ -1,24 +1,20 @@
-package WeeklyChallenge_4.Mr;
+package WeeklyChallenge_4;
 
-
-
-public class Stark {
-    public static double sol(double x,double y,double z) {
-        if (x > y) {
-            double PA, AB, BM = 0;
-            PA = y / 5;
-            AB = y / 3;
-            BM = z - PA - AB;
-            x = x - 2 * y;
-            double ans = Math.abs(x - BM);
-            return ans;
+public class Stark
+{
+        public static void main(String[] args)
+        {
+            double average=(weaponsDel(500,1000,100)+weaponsDel(200,100,125)+weaponsDel(3000,1000,1000)+weaponsDel(25000,10000,12000)+weaponsDel(12345,2120,1111))/5.0;
+            System.out.println(average);
         }
-        return x - z > 0 ? x - z : 0;
-    }
-    public static void main(String[] args)
-    {
-        System.out.print((int)sol(3000,1000,1000) + sol(200,100,125) + sol(500,1000,100)+ sol(25000,10000,12000));
-    }
-
+        public static int weaponsDel(int X, int Y, int Z)
+        {
+            int maxDistance=Math.round(X/Y);
+            int sum=0;
+            for(int i=1;i<=maxDistance;i++){
+                sum+=Math.round(Y/(2*i-1));
+            }
+            sum+=Math.round((X%Y)/(2*maxDistance+1));
+            return sum-Z+1;
+        }
 }
-
